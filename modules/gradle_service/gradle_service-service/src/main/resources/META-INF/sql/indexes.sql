25da03612e45cf7d7d9bd018ca4e6e097e2e583a
SESA612648 <SESA612648@WTIN01505169L.apa.gad.schneider-electric.com>
2021-03-18 11:54:02 +0530

    Testing with modules

---
+++ modules/gradle_service/gradle_service-service/src/main/resources/META-INF/sql/indexes.sql
@@ -0,0 +1,3 @@
+create index IX_C53FA3AB on SE_Foo (field2);
+create index IX_E4545EF3 on SE_Foo (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_753E0635 on SE_Foo (uuid_[$COLUMN_LENGTH:75$], groupId);
---
+++ modules/test/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,6 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="test.caption"/></b>
+	<h1> Testing gradle</h1>
+</p>
